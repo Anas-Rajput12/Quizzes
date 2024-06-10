c0a4ca5e31c32b39d2849d5b034c6c33e962202f
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2024-06-10 17:47:15 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,107 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const quizzes: {
+  Question_1: string;
+  Question_2: string;
+  Question_3: string;
+  Question_4: string;
+  Question_5: string;
+} = await inquirer.prompt([
+  {
+    type: "list",
+    name: "Question_1",
+    choices: ["a < b", "a > b", "a !== b", "a === b"],
+    message:
+      chalk.bold.greenBright`Q:1 In Typescript, What is the correct syntax to Check a is Greater than b ?`,
+  },
+  {
+    type: "list",
+    name: "Question_2",
+    choices: ["string", "number", "boolean", "Array"],
+    message: chalk.bold.greenBright`Q:2 In TypeScript, which data types used to print the name ?`,
+  },
+  {
+    type: "list",
+    name: "Question_3",
+    choices: [":", ".", ";", ","],
+    message:
+    chalk.bold.greenBright`Q:3 In TypeScript, which symbol is commonly used to terminate a statement ?`,
+  },
+  {
+    type: "list",
+    name: "Question_4",
+    choices: ["&", "$", "@", "#"],
+    message:
+    chalk.bold.greenBright`Q:4 In TypeScript, Which of the following characters is commonly allowed in variable names in typescript ?`,
+  },
+  {
+    type: "list",
+    name: "Question_5",
+    choices: ["prompt()", "start()", "init()", "run()"],
+    message:
+    chalk.bold.greenBright`Q:5 In TypeScript, Which method of inquirer.js is used to start the prompt interface and recieve user input ?`,
+  },
+]);
+
+let score: number = 0;
+
+switch (quizzes.Question_1) {
+    case "a > b":
+        console.log(chalk.bold.green`Correct Answer`);
+        ++score;
+        break;
+
+    default:
+        console.log(chalk.bold.red.italic`Wrong Answer (Correct Answer is a > b)`);
+        break;
+}
+console.log(chalk.bold.blue`Score question 1: ${score}`);
+
+switch (quizzes.Question_2) {
+    case "string":
+        console.log(chalk.bold.green`Correct Answer`);
+        ++score;
+        break;
+
+    default:
+        console.log(chalk.bold.red.italic`Wrong Answer (Correct Answer is string)`);
+        break;
+}
+console.log(chalk.bold.blue`Score question 2: ${score}`);
+
+switch (quizzes.Question_3) {
+    case ";":
+        console.log(chalk.bold.green`Correct Answer`);
+        ++score;
+        break;
+
+    default:
+        console.log(chalk.bold.red.italic`Wrong Answer (Correct Answer is ;)`);
+        break;
+}
+console.log(chalk.bold.blue`Score question 3: ${score}`);
+
+switch (quizzes.Question_4) {
+    case "$":
+        console.log(chalk.bold.green`Correct Answer`);
+        ++score;
+        break;
+
+    default:
+        console.log(chalk.bold.red.italic`Wrong Answer(Correct answer is $)`);
+        break;
+}
+console.log(chalk.bold.blue`Score question 4: ${score}`);
+
+switch (quizzes.Question_5) {
+    case "prompt()":
+        console.log(chalk.bold.green`Correct Answer`);
+        ++score;
+        break;
+
+    default:
+        console.log(chalk.bold.red.italic`Wrong Answer (Correct Answer is prompt())`);
+        break;
+}
+console.log(chalk.bold.blue`Final score: ${score}`);
